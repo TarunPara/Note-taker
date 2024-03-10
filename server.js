198ea448998a454fc02d77f803474791ee7fbef6
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-10 07:17:07 -0400

    done

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+  
