f6472a7c49f44f3416026eb38667954950d5e3c6
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-10 07:54:01 -0400

    creating neew files assest not working

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 1234;
+
+// Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
-  
+  console.log(`Server is listening on port ${PORT}`);
+});
